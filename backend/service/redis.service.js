2a99f4361d40674bdf748131e2dba7ff851a9f04
rei_kaji <st.r.z.rei@gmail.com>
2023-02-07 19:40:31 -0800

    update redis

--- backend/service/redis.service.js
+++ backend/service/redis.service.js
@@ -1,12 +1,15 @@
 import redis from "redis";
+import ioRedis from "ioredis";
 import dotenv from "dotenv";
 
 dotenv.config();
 console.log("process.env.REDIS_HOST: ", process.env.REDIS_HOST);
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-});
+// const redisClient = redis.createClient({
+//   host: process.env.REDIS_HOST,
+//   port: process.env.REDIS_PORT,
+// });
+
+const redisClient = new ioRedis(process.env.REDIS_URL);
 
 redisClient.on("connect", () => {
   console.log("Redis client connected");
