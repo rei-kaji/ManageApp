4283a48f3b8cd3205bc723fded91c7e0d5c61cc4
rei_kaji <st.r.z.rei@gmail.com>
2023-02-11 02:56:11 -0800

    update sampleUrl

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -28,6 +28,9 @@ const radios = [
 const avatarSample =
   "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80";
 
+const sampleUrl =
+  "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80";
+
 const agencyInfoGroup = [
   {
     label: "Agency Name",
